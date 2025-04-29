fc524e2b0d890a64e482619806ab28a11be238c6
asalaskar566 <apekshasalaskar3@gmail.com>
2025-04-29 23:24:34 +0530

    Add files via upload

---
+++ GRADE.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main ()
+{
+	int marks;
+	printf ( "Enter Marks\t:");
+	scanf("%d",&marks);
+	if (marks>=90)
+	{ printf ("EX");
+	
+	}
+	else if (marks >=80)
+	{  
+	printf ("A");
+	
+	}
+	else if (marks >=70)
+	{ 
+	printf ("B");
+	
+	}
+	else if (marks >60)
+	{ 
+	printf ("C") ;
+	
+    }
+    else if (marks>50)
+    { 
+    printf("D");
+	
+	}
+	else if (marks>40)
+	{
+    printf ("E");
+    
+ 	}
+
+}
+
+
+
+	
+  
+
