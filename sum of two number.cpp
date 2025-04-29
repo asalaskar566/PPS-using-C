3b8674a8e2e7ae84824514533248d46bdf7261b2
asalaskar566 <apekshasalaskar3@gmail.com>
2025-04-29 21:06:59 +0530

    Add files via upload

---
+++ sum of two number.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{
+int num1, num2, sub;
+
+printf( " Please Enter num1");
+scanf ( "%d",&num1);
+printf(" please Enter num2");
+scanf ("%d",&num2);
+sub= num1-num2;
+printf (" * The sub of %d and %d is %d ",num1,num2,sub);
+return 0 ;
+}
