3b8671802d131952565cfd8a0552354431ae0617
asalaskar566 <apekshasalaskar3@gmail.com>
2025-04-29 21:07:44 +0530

    Add files via upload

---
+++ Multiplication of two number.cpp
@@ -0,0 +1,13 @@
+# include <stdio.h>
+int main ()
+{
+int num1, num2, MUL;
+
+printf( " Please Enter num1");
+scanf ( "%d",&num1);
+printf(" please Enter num2");
+scanf ("%d",&num2);
+MUL= num1*num2;
+printf (" * The sub of %d and %d is %d ",num1,num2,MUL);
+return 0 ;
+}
