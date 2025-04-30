755838736c4ffa6b70f82b106e0841c446b5ecf2
asalaskar566 <apekshasalaskar3@gmail.com>
2025-04-30 16:41:50 +0530

    Add files via upload

---
+++ Average of two number.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2,avg;
+	printf("Enter First Number :");
+	scanf("%d",&num1);
+	printf("\nEnter Second Number :");
+	scanf("%d",&num2);
+	avg =( num1+num2)/2;
+	printf("\nAverage of %d and %d id %d",num1,num2,avg);
+	return 0;
+}
+
