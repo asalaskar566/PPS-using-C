8bd60d530160f8f115021ca5cb37e91e75e78ab3
asalaskar566 <apekshasalaskar3@gmail.com>
2025-04-30 16:41:20 +0530

    Add files via upload

---
+++ Even And Odd.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+	int num1;
+	printf("Enter The Number :");
+	scanf("%d",&num1);
+	if( num1%2==0)
+	{
+		printf("\nNumber is Even");
+		
+	}
+	else
+	{
+		printf("\nNumber is ODD");
+		
+	}
+	return 0;
+	
+}
