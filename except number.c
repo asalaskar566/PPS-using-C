5ecd27a65fad57753a057e39eb17860d41afb06f
asalaskar566 <apekshasalaskar3@gmail.com>
2025-07-17 23:50:01 +0530

    Add files via upload

---
+++ except number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    
+    printf("Enter the number: ");
+    scanf("%d", &number);  
+
+    
+    printf("You entered number: %d\n", number);
+
+    return 0;
+}
